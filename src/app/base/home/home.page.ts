0867883e0e7c6bd8ace7c0e901d2964d2e4a3193
CETECOM <33809410+TulioTriste@users.noreply.github.com>
2024-10-11 11:11:16 -0300

    a

--- src/app/base/home/home.page.ts
+++ src/app/base/home/home.page.ts
@@ -8,7 +8,9 @@ import { NavController } from '@ionic/angular';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-username = '';
+
+  username = '';
+
   constructor(private router: Router, private navCtrl: NavController) {
     const navegacion = this.router.getCurrentNavigation();
     const state = navegacion?.extras.state as {
@@ -21,6 +23,19 @@ username = '';
   }
 
   ngOnInit() {}
-  
+
+  goPerfil() {
+    let navigationExtras = {
+      state: {
+        username: this.username,
+        //category: this.user.category,
+      },
+    };
+    this.navCtrl.navigateForward('/perfil', {
+      animated: true,
+      animationDirection: 'forward', // Establecemos la animación de deslizamiento hacia adelante
+      state: navigationExtras.state,  // Pasamos el estado (datos)
+    });
+  }
 }
 
--- src/app/perfil/perfil.page.ts
+++ src/app/perfil/perfil.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-perfil',
@@ -7,7 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PerfilPage implements OnInit {
 
-  constructor() { }
+  username = '';
+  qrData: string = 'https://www.google.com';
+
+  constructor(private router: Router, private navCtrl: NavController) {
+    const navegacion = this.router.getCurrentNavigation();
+    const state = navegacion?.extras.state as {
+      username: '';
+      password: '';
+    };
+    this.username = state.username;
+  }
 
   ngOnInit() {
   }
